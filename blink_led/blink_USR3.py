334cee3daf7ef0fe1e84c887e4b1b7c7c1038fb1
Demo User <debian@beaglebone.localdomain>
2024-02-21 03:54:12 +0000

    adding files

--- blink_led/blink_USR3.py
+++ blink_led/blink_USR3.py
@@ -3,7 +3,7 @@
 Blinking USER3 LED
 --------------------------------------------------------------------------
 License:   
-Copyright 2021-2024 - Ibrahim Muzammil
+Copyright 2021-2024 - Ibrahim Muzammil (im33@rice.edu)
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
